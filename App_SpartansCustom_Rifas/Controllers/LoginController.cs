24f3ed52e572f45e0b1366a5ff879731b056067b
selvinmedina <selvinmedina0016@gmail.com>
2020-03-24 19:38:04 -0600

    creacion de archivos

---
+++ App_SpartansCustom_Rifas/Controllers/LoginController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace App_SpartansCustom_Rifas.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult Register()
+        {
+            return View();
+        }
+    }
+}
---
+++ App_SpartansCustom_Rifas/Scripts/app/seguridad/login.js
@@ -0,0 +1,11 @@
+$('#useremail').change(function (e) {
+    var emailRegex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    var EmailId = this.value;
+    if (emailRegex.test(EmailId))
+        console.log('Email valido');
+
+    else
+        console.log('Email invalido');
+
+
+});
